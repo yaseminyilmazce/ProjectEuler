0119f6aff5443a7e414b82bcffae0b8da9943a40
Yasemin <yaseminyilmaziu@gmail.com>
2014-08-11 23:31:40 +0300

    Update P2.c

--- P2.c
+++ P2.c
@@ -1,25 +1,29 @@
-// 374925503 BULUYOR AMA YANLIŞ 
-
-
-
-#include <stdio.h>
-
 int main(int argc, char *argv[]) {
 	
-	unsigned long int toplam,t1,t2,cnt;
-	t1=0;
-	t2=1;
+	unsigned long int toplam,t1,t2,cnt,even;
+	t1=1;
+	t2=2;
+	even=0;
 	cnt=0;
-	while(toplam!=4000000)
+	while(toplam<4000000)
 	{
 		toplam=t1+t2;
 		t1=t2;
 		t2=toplam;
 		
+		
 		cnt++;
+	    if(toplam%2==0)
+		{
+			even=even+toplam;
+			
+		}	
+		
+		
 	}
 	
-	printf("%ld ",cnt);
+	
+	printf("%ld ",even);
 	
 	
 	
